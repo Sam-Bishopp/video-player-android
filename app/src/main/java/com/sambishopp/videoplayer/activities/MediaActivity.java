06414a6aae417206c25ac33022dba73262869f93
Sam Bishopp <sambishopp7@gmail.com>
2021-03-13 01:51:41 +0000

    Requesting permissions for storage access in MediaActivity.

--- app/src/main/java/com/sambishopp/videoplayer/activities/MediaActivity.java
+++ app/src/main/java/com/sambishopp/videoplayer/activities/MediaActivity.java
@@ -1,19 +1,88 @@
 package com.sambishopp.videoplayer.activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.sambishopp.videoplayer.R;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 /**
  * Activity to play show available media from a specific folder.
  */
 public class MediaActivity extends AppCompatActivity {
 
+    private int STORAGE_PERMISSION_CODE = 100;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
+
+        checkStoragePermission();
+    }
+
+    private void checkStoragePermission()
+    {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(MediaActivity.this, "You already granted this permission", Toast.LENGTH_SHORT).show();
+            /**createDirectory(); */ //Add this function
+        }
+        else
+        {
+            requestStoragePermission();
+        }
+    }
+
+    private void requestStoragePermission()
+    {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE))
+        {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Needed")
+                    .setMessage("Please grant permission to allow the app to play videos")
+                    .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            ActivityCompat.requestPermissions(MediaActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Reject", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if(requestCode == STORAGE_PERMISSION_CODE)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
